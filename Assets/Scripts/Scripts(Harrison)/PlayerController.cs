98a691c676c1b5eea48041456f987cdde71e25c2
IchorLQ <hbbooker@gmail.com>
2017-01-21 10:44:36 -0800

    added personal test scene and player controller script

---
+++ Assets/Scripts/Scripts(Harrison)/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		other.SendMessage ("OnHit", SendMessageOptions.DontRequireReceiver);
+		Debug.Log ("player hit");
+		Destroy (other.gameObject);
+	}
+
+	void Update () {
+		
+	}
+}
