77898f7811b9bb9ae3c2c573c0f26dc9d94df6fd
IchorLQ <hbbooker@gmail.com>
2017-01-21 10:09:36 -0800

    player & shield prefabs w/ shield activation and collision detection

---
+++ Assets/Scripts/Scripts(Harrison)/ShieldController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldController : MonoBehaviour {
+	public KeyCode activateOn = KeyCode.None;
+
+	SpriteRenderer render;
+	Color activeColor;
+	Color passiveColor;
+	bool isActive = false;
+
+	// Use this for initialization
+	void Start () {
+		render = GetComponent<SpriteRenderer> ();
+		activeColor = render.color;
+		passiveColor = new Color(activeColor.r, activeColor.g, activeColor.b, 0);
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		if (isActive) {
+			other.SendMessage ("OnBlock", SendMessageOptions.DontRequireReceiver);
+			Debug.Log ("blocked");
+			//Destroy (other.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isActive = Input.GetKey (activateOn);
+
+		if (isActive) {
+			render.color = activeColor;
+		} else {
+			render.color = passiveColor;
+		}
+	}
+}
