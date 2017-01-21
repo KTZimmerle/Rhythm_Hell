2e442e865ebb2e52c94ffe411dfa9f519c7697c6
IchorLQ <hbbooker@gmail.com>
2017-01-21 14:35:31 -0800

    added main menu scene
    canvas>startbutton object int the hierarchy has a public property for
    which scene to load

---
+++ Assets/Scripts/Scripts(Harrison)/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+	public string sceneToLoad = "Harrison(Test)";
+
+	void Start () {
+		
+	}
+
+	void OnStart() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene (sceneToLoad);
+	}
+
+	void Update () {
+		
+	}
+}
