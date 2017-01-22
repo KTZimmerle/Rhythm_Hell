baf54fe731fd71dd8141cdf9551c88fb8d5c0c94
KTZimmerle <kzimmerl@uci.edu>
2017-01-21 18:36:17 -0800

    Added Boss script. Currently bare bones

--- Assets/Scripts/Scripts(Kevin)/RH_BeatDetector.cs
+++ Assets/Scripts/Scripts(Kevin)/RH_BeatDetector.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SSTiming : MonoBehaviour {
+public class RH_BeatDetector : MonoBehaviour {
 
+    const int SAMPLE_SIZE = 1024;
+    int samples = 44100;
+    
 	// Use this for initialization
 	void Start () {
 		
---
+++ Assets/Scripts/Scripts(Kevin)/RH_Boss.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RH_Boss : MonoBehaviour {
+
+    public int health = 20;
+    RH_ScoreSystem ss;
+
+	// Use this for initialization
+	void Awake () {
+        ss = GameObject.FindGameObjectWithTag("ScoreSystem").GetComponent<RH_ScoreSystem>();
+	}
+
+    // Unknown if boss does anything yet
+    /*void Update () {
+		
+	}*/
+
+    public void takeDamage(int dmg)
+    {
+        health -= dmg;
+        if (health < 0)
+        {
+            //boss dies, gain score
+            ss.ModifyScore(1000);
+            Destroy(gameObject);
+        }
+    }
+}
