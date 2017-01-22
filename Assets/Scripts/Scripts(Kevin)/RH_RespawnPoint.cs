013c1fcf3450976e221ca8d4434ef423b58b7ec1
KTZimmerle <kzimmerl@uci.edu>
2017-01-22 14:49:46 -0800

    Enemy fade now works properly

--- Assets/Scripts/Scripts(Kevin)/RH_RespawnPoint.cs
+++ Assets/Scripts/Scripts(Kevin)/RH_RespawnPoint.cs
@@ -74,12 +74,11 @@ public class RH_RespawnPoint : MonoBehaviour {
     {
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
         Color temp = sr.color;
-        float alpha = temp.a;
         for (int i = 0; i < 120; i++)
         {
-            temp.a = (119 - i) / 119;
+            temp.a = (119.0f - i) / 119;
             sr.color = temp;
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.017f);
         }
         yield return new WaitForSeconds(2.0f);
     }
