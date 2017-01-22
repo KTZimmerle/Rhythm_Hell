532a566e5ae77218474de2498ed17f15630f3070
KTZimmerle <kzimmerl@uci.edu>
2017-01-22 10:13:26 -0800

    Added in scrolling background

---
+++ Assets/Scripts/Scripts(Kevin)/RH_ParallexScroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RH_ParallexScroll : MonoBehaviour {
+
+    public float speed;
+
+    Vector2 offset;
+    float lastX;
+
+    void Start()
+    {
+        offset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset("_MainTex");
+        lastX = offset.x;
+    }
+
+    void Update()
+    {
+        float x = Mathf.Repeat(Time.deltaTime * speed + lastX, 1);
+        Vector2 newOffSet = new Vector2(x, offset.y);
+        lastX = newOffSet.x;
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", newOffSet);
+    }
+}
