a9c9e46790c0a58442c8610c3858142320571590
Tom Tan <tomtan117@gmail.com>
2017-01-22 14:14:45 -0800

    Audio Pause When in pause menu

--- Assets/Scripts/Scripts (Tom)/AudioDelay.cs
+++ Assets/Scripts/Scripts (Tom)/AudioDelay.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class AudioDelay : MonoBehaviour {
     public float timer;
+    private bool pause = false;
 	// Use this for initialization
 	void Start () {
         Invoke("PlayAudio", timer);
 	}
 	
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pause == false)
+            {
+                pause = true;
+                GetComponent<AudioSource>().Pause();
+            }
+            else
+            {
+                pause = false;
+                GetComponent<AudioSource>().Play();
+            }
+        }
+    }
 	void PlayAudio()
     {
         GetComponent<AudioSource>().Play();
