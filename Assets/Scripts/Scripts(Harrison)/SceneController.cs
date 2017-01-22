4e48a1bb93d9c896c735058aebe0a12fa8feb5e7
IchorLQ <hbbooker@gmail.com>
2017-01-22 11:10:23 -0800

    win/loss mutual exclusion and main menu update

--- Assets/Scripts/Scripts(Harrison)/SceneController.cs
+++ Assets/Scripts/Scripts(Harrison)/SceneController.cs
@@ -6,31 +6,36 @@ public class SceneController : MonoBehaviour {
 	public string sceneToLoad = "Main Menu";
 
 	bool checkWin = false;
+	bool gameOver = false;
 	UnityEngine.UI.Text overlayText;
 	TransitionButton transButton;
 
 	void Start () {
-		//UnityEngine.UI.Text[] texts = FindObjectsOfType<UnityEngine.UI.Text> ();
-
 		overlayText = FindObjectOfType<OverlayController> ().GetComponent<UnityEngine.UI.Text>();
 		transButton = FindObjectOfType<TransitionButton> ();
 	}
 
 	void NextScene() {
-		Debug.Log ("changing to next scene " + sceneToLoad);
+		//Debug.Log ("changing to next scene " + sceneToLoad);
 		UnityEngine.SceneManagement.SceneManager.LoadScene (sceneToLoad);
 	}
 
 	void GameOverLose() {
-		overlayText.text = "Game Over!";
-		overlayText.enabled = true;
-		transButton.SendMessage ("EnableButton");
+		if (!gameOver) {
+			gameOver = true;
+			overlayText.text = "Game Over!";
+			overlayText.enabled = true;
+			transButton.SendMessage ("EnableButton");
+		}
 	}
 
 	void GameOverWin() {
-		overlayText.text = "Victory!";
-		overlayText.enabled = true;
-		transButton.SendMessage ("EnableButton");
+		if (!gameOver) {
+			gameOver = true;
+			overlayText.text = "Victory!";
+			overlayText.enabled = true;
+			transButton.SendMessage ("EnableButton");
+		}
 	}
 
 	void BeatListDone() {
