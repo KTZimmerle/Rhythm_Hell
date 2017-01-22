8b73cebe892f553e8c02c37c0c0a236512cb178f
KTZimmerle <kzimmerl@uci.edu>
2017-01-21 19:05:24 -0800

    Added in method in EnergyBarController. Read Description
    It consumes all energy in bar, but destroys all waves and hurts the
    boss.

--- Assets/Scripts/Scripts(Harrison)/EnergyBarController.cs
+++ Assets/Scripts/Scripts(Harrison)/EnergyBarController.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnergyBarController : MonoBehaviour {
-	public float maxEnergy = 100.0f;
-	public float minEnergy = 0.0f;
-	public float incrementAmount = 10.0f;
-	public float decrementAmount = 15.0f;
+public class EnergyBarController : MonoBehaviour
+{
+    public float maxEnergy = 100.0f;
+    public float minEnergy = 0.0f;
+    public float incrementAmount = 10.0f;
+    public float decrementAmount = 15.0f;
 
-	UnityEngine.UI.Image img;
+    RH_ScoreSystem ss;
+    RH_Boss boss;
+    public KeyCode activateOn = KeyCode.None;
+
+    UnityEngine.UI.Image img;
 	float currentEnergy = 0.0f;
 
 	void Start () {
+        boss = GameObject.FindGameObjectWithTag("Boss").GetComponent<RH_Boss>();
+        ss = GameObject.FindGameObjectWithTag("ScoreSystem").GetComponent<RH_ScoreSystem>();
 		img = GetComponent<UnityEngine.UI.Image> ();
 		img.fillAmount = currentEnergy / maxEnergy;
 	}
@@ -28,13 +35,33 @@ public class EnergyBarController : MonoBehaviour {
 		img.fillAmount = currentEnergy / maxEnergy;
 	}
 
-	public void DecrementEnergy()
-	{
-		currentEnergy = Mathf.Max (currentEnergy - decrementAmount, minEnergy);
-		img.fillAmount = currentEnergy / maxEnergy;
-	}
+    public void DecrementEnergy()
+    {
+        currentEnergy = Mathf.Max(currentEnergy - decrementAmount, minEnergy);
+        img.fillAmount = currentEnergy / maxEnergy;
+    }
 
+    void Update () {
 
-	void Update () {
-	}
+        if (Input.GetKeyDown(activateOn) && currentEnergy >= maxEnergy)
+        {
+            StartCoroutine(ActivateAttack());
+        }
+    }
+
+    IEnumerator ActivateAttack()
+    {
+        GameObject[] waves = new GameObject[50];
+        waves = GameObject.FindGameObjectsWithTag("waves");
+        foreach(GameObject w in waves)
+        {
+            Destroy(w);
+            ss.ModifyScore(100);
+        }
+        boss.takeDamage(2);
+        currentEnergy = 0.0f;
+        img.fillAmount = currentEnergy / maxEnergy;
+
+        yield return new WaitForSeconds(0.1f);
+    }
 }
