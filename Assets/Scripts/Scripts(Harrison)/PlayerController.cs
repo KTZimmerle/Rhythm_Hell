930f0cb133c51b68967fa199f58440aac82f3185
IchorLQ <hbbooker@gmail.com>
2017-01-21 20:39:26 -0800

    victory condition

--- Assets/Scripts/Scripts(Harrison)/PlayerController.cs
+++ Assets/Scripts/Scripts(Harrison)/PlayerController.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	public int hitPoints = 10;
-	public int damagePerHit = 1;
 	EnergyBarController eBar;
 
 	void Start () {
@@ -15,13 +13,6 @@ public class PlayerController : MonoBehaviour {
 		Debug.Log ("player hit");
 		Destroy (other.gameObject);
 		eBar.DecrementEnergy ();
-		hitPoints -= damagePerHit;
-		//also reduce the energy bar if hit
-
-
-		if (hitPoints <= 0) {
-			FindObjectOfType<SceneController> ().SendMessage ("GameOverLose");
-		}
 	}
 
 	void Update () {
