254e9b25954f78471a34087d5285346419ae1619
Tom Tan <tomtan117@gmail.com>
2017-01-21 20:57:40 -0800

    Added Audio Delay Script

---
+++ Assets/Scripts/Scripts(Tom)/TriggerAudio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudio : MonoBehaviour {
+    public float timer = 0;
+    // Use this for initialization
+    void Start() {
+        Invoke("PlayAudio", timer);
+    }
+
+    void PlayAudio()
+    {
+        GetComponent<AudioSource>().Play();
+        CancelInvoke();
+    }
+}
+	
